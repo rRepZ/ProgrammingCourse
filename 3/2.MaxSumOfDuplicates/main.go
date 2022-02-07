d7a6874aa31026b978fbc6471fe1ac152ce47098
xRepZ <zxbnyby@yahoo.com>
2022-02-07 20:23:43 +0300

    Maximum sum

---
+++ 3/2.MaxSumOfDuplicates/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func MaxSum(thisSlice []int) {
+	resultMap := make(map[int]int)
+	for i := 0; i < len(thisSlice); i++ {
+		if resultMap[thisSlice[i]] >= thisSlice[i] {
+			resultMap[thisSlice[i]] += thisSlice[i]
+		} else {
+			resultMap[thisSlice[i]] = thisSlice[i]
+		}
+	}
+	fmt.Println(resultMap)
+}
+
+func main() {
+	mass := []int{1, 2, 3, 4, 5, 2, 6, 7, 8, 8}
+	MaxSum(mass)
+}
